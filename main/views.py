be2c7bc308b0fd7949a15fcb9d883a9b25d6e7aa
admin <tanyabondar3011@gmail.com>
2024-12-11 17:30:19 +0200

    First simple example

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request) -> HttpResponse:
+    return render(request, 'main/index.html')
+
+def about(request) -> HttpResponse:
+    return HttpResponse('About page')
