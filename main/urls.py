057edc745ecb66724d079ccc856f2f41c39749b8
admin <tanyabondar3011@gmail.com>
2025-01-23 20:21:12 +0200

    Editing the footer, adding information and creating relevant links

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import PrivacyPolicyView, TermsAndConditionsView
 from main import views
 
 app_name = "main"
@@ -7,4 +7,6 @@ app_name = "main"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms-and-conditions'),
 ]
--- main/views.py
+++ main/views.py
@@ -11,7 +11,7 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home - Главная'
-        context['content'] = "Магазин мебели HOME"
+        context['content'] = "Магазин мебели HomeMade"
         return context
 
 
@@ -20,9 +20,27 @@ class AboutView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home - О нас'
-        context['content'] = "О нас"
-        context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        context['title'] = 'Home - Про нас'
+        context['content'] = "Про нас"
+        context['text_on_page'] = "Наш мебельный магазин — это сочетание стиля, комфорта и качества. Мы предлагаем мебель, созданную для вашего уюта: от изысканных диванов до функциональных обеденных столов. Каждое изделие выполнено из качественных материалов и продумано до мелочей, чтобы радовать вас долгие годы. У нас вы найдёте стильные решения для любого интерьера, а опытные консультанты помогут выбрать то, что идеально подойдёт для вашего дома. Мы делаем ваш уют доступным!"
+        return context
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'main/privacy_policy.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['site_name'] = "HomeMade"
+        return context
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = 'main/terms_and_conditions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['site_name'] = "HomeMade"
         return context
 
 
